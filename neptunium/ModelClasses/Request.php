bddfdb9c18e973d1f6d98871e04fc3b4aa3fd405
szczypiorofix <szczypiorofix@o2.pl>
2024-07-15 13:37:37 +0200

    Add request and response classes

---
+++ neptunium/ModelClasses/Request.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Neptunium\ModelClasses;
+
+class Request {
+    private array $headers;
+    private string $method;
+    private string $url;
+    private ?string $body = null;
+
+    public function __construct() {
+        $this->url = $this->resolveUrl();
+        $this->method = $this->resolveMethod();
+        $this->headers = array_change_key_case(
+            getallheaders(),
+            CASE_LOWER
+        );;
+    }
+
+    private function resolveUrl(): string {
+        $filteredUrl = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_STRING);
+        if ($filteredUrl) {
+            return rtrim($filteredUrl);
+        }
+        return '';
+    }
+
+    private function resolveMethod(): string {
+        return ($_SERVER["REQUEST_METHOD"]);
+    }
+
+    public function getHeaders(): array {
+        return $this->headers;
+    }
+
+    public function getMethod(): string {
+        return $this->method;
+    }
+
+    public function getUrl(): string {
+        return $this->url;
+    }
+
+    public function getBody(): string {
+        return $this->body;
+    }
+}
