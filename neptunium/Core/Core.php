a386d3b5fdaf599d6428e24ae173181628c2d941
szczypiorofix <szczypiorofix@o2.pl>
2024-07-16 08:46:23 +0200

    Refactor of main core parts

--- neptunium/Core/Core.php
+++ neptunium/Core/Core.php
@@ -4,11 +4,15 @@ namespace Neptunium\Core;
 
 use Neptunium\Controllers\HomeController;
 use Neptunium\Controllers\MainController;
+use Neptunium\ModelClasses\Request;
+use Neptunium\ModelClasses\Response;
 
 class Core {
-    private ?Environment $environment;
-    private Dotenv $dotenv;
+    private Environment $environment;
+    private DatabaseConnection $databaseConnection;
     private Router $router;
+    private Request $request;
+    private Response $response;
 
     public function __construct(
         private readonly string $rootDir,
@@ -18,28 +22,36 @@ class Core {
     private function __clone() {}
 
     public function launch(): void {
+        $this->prepareEnvironment();
+        $this->prepareDatabaseConnection();
+        $this->prepareRouter();
+        $this->resolveRequest();
+
+        $pageContent = $this->handleRoutes();
+
+        $this->resolveResponse($pageContent);
+    }
+
+    private function prepareEnvironment(): void {
         $this->environment = new Environment(
             $this->rootDir,
-            $this->appRootDir,
-            [
-                "DB_NAME",
-                "DB_HOST",
-                "DB_USER",
-                "DB_PASS",
-            ]
+            $this->appRootDir
         );
+        $requiredEnvironmentalVariableKeys = [
+            "DB_NAME",
+            "DB_HOST",
+            "DB_USER",
+            "DB_PASS",
+        ];
 
         try {
-            $this->environment->loadDotEnv();
+            $this->environment->loadDotEnv($requiredEnvironmentalVariableKeys);
         } catch (\Exception $e) {
             echo 'An error occurred while loading environmental variables: '. $e->getMessage();
         }
+    }
 
-        $db = DatabaseConnection::getConnection();
-
-        Registry::add($db, "database connection");
-        Registry::add($this->environment, "Environment");
-
+    private function prepareRouter(): void {
         $this->router = new Router();
         try {
             $this->router->registerRoutesFromControllerAttributes(
@@ -51,35 +63,33 @@ class Core {
         } catch (\ReflectionException $e) {
             echo 'An error occurred while registering routes: '. $e->getMessage();
         }
-
-        Registry::add($this->router, "Router");
-        Registry::remove("Environment");
-
-        $urlPath = $this->parseUrl();
-        $pageContent = $this->router->handleRoutes($urlPath);
-
-//        $this->dump(Registry::returnList());
-
-        echo $pageContent;
     }
 
-    public function getRouter(): Router {
-        return $this->router;
+    private function prepareDatabaseConnection(): void {
+        $this->databaseConnection = DatabaseConnection::getConnection();
     }
 
-    public function getEnvironment(): Environment {
-        return $this->environment;
+    private function resolveRequest(): void {
+        $this->request = new Request();
     }
 
-    private function parseUrl(): string {
-        $filteredUrl = filter_input(INPUT_GET, 'url', FILTER_SANITIZE_STRING);
-        if ($filteredUrl) {
-            return rtrim($filteredUrl);
+    private function handleRoutes(): string {
+        return $this->router->handleRoutes(
+            $this->request->getMethod(),
+            $this->request->getUrl()
+        );
+    }
+
+    private function resolveResponse(string $pageContent): void {
+        $this->response = new Response();
+        $this->response->setContent($pageContent);
+        $this->response->viewPageContent();
+    }
+
+    private function dump(mixed $data, string $name = ''): void {
+        if ($name) {
+            echo "<p>$name</p>";
         }
-        return '';
-    }
-
-    private function dump(mixed $data): void {
         echo '<pre>';
         print_r($data);
         echo '</pre>';
